de665f61d5debd7e212582911cd6dd8a9865e805
sandras1 <sandra@techversantinfo.com>
2025-07-17 11:27:03 +0530

    Added .gitignore

--- apps/payments/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def accounts_home(request):
-    return render(request, 'accounts/admin_dashbord.html')
--- apps/subscriptions/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.accounts_home, name='accounts_home'),
-]
