f2737d8c7cf7037a9e0216f797281142bc6ef462
sandras1 <sandra@techversantinfo.com>
2025-07-31 20:42:27 +0530

    Fixed bugs

--- cable_operator/settings.py
+++ cable_operator/settings.py
@@ -27,7 +27,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'accounts.AdminUser'
